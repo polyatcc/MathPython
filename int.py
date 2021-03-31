13c9a8c0cfcdfe2f6f0f38a97154a01b142a1fc2
Polina Tceneva <59669268+polyatcc@users.noreply.github.com>
2021-04-01 02:13:58 +0300

    Add files via upload

--- int.py
+++ int.py
@@ -1,38 +1,48 @@
 import math
 
 
-def dichotomy(f, a, b, eps):
+def gold_section(f, a, b, eps):
     list = []
     count = 0
     v = 0
+    const = (3 - math.sqrt(5)) / 2
+    x1 = a + const * (b - a)
+    x2 = b - const * (b - a)
+    y1 = f(x1)
+    y2 = f(x2)
+    v += 2
+    list.append({a, b})
     while abs(b - a) > eps:
         count += 1
-        x1 = (b + a) / 2 - eps / 4
-        x2 = (b + a) / 2 + eps / 4
-        if f(x1) == f(x2):
-            v += 2
-            a = x1
+        if y1 < y2:
             b = x2
+            x2, y2 = x1, y1
+            x1 = a + const * (b - a)
+            y1 = f(x1)
+            v += 1
             list.append({a, b})
-            continue
-        if f(x1) > f(x2):
-            a = x1
         else:
-            b = x2
-        v += 2
-        list.append({a, b})
-    return (a + b) / 2, count, v, list
+            a = x1
+            x1, y1 = x2, y2
+            x2 = b - const * (b - a)
+            y2 = f(x2)
+            v += 1
+            list.append({a, b})
+    return (x1 + x2) / 2, count, v, list
 
 
 def a(x):
     return math.log(x, 10) * math.sin(x) * x ** 2
 
-
-i = 0.000001
 '''
+i = 0.1
+
 for j in range(9):
     print(i)
-    print(dichotomy(a, 0, 1, i))
+    print(gold_section(a, 0, 1, i))
     i /= 10
 '''
-print(dichotomy(a, 0, 1, i))
+j = 0.000001
+print(gold_section(a, 0, 1, j))
+
+
